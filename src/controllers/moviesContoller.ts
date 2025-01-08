7848064c898e21c6e6da863b273fb5aa4ecd820f
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-01-08 15:08:17 +0100

    fix

--- src/controllers/moviesContoller.ts
+++ src/controllers/moviesContoller.ts
@@ -52,7 +52,7 @@ export const addMovie = async (req: Request, res: Response) => {
     );
   }
 
-  res.status(201).json({ movie });
+  res.status(201).json({ message: "Movie added successfully", movie });
 };
 
 export const getAllMovies = async (req: Request, res: Response) => {
@@ -84,6 +84,8 @@ export const getAllMovies = async (req: Request, res: Response) => {
 };
 
 export const updateMovie = async (req: Request, res: Response) => {
+  const { title, description, posterImg, genres, showtimes } = req.body;
+
   res.status(200).json("updated");
 };
 
